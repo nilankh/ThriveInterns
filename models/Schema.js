f11c79ec65239a32e540bfa1fcfb6b712f6e6114
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-01-13 12:16:29 +0530

    Created Schema

---
+++ models/Schema.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const BankSchema = mongoose.Schema({
+  ifsc: {
+    type: String,
+    required: true,
+  },
+  bank_id: {
+    type: Number,
+    required: true,
+  },
+  branch: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  district: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  bank_name: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("bank", BankSchema);
