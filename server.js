3b4c995cc859709cc47732be45b8ca4bd6b30a6d
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-01-13 12:06:10 +0530

    connected with database

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 // Create express app
 const app = express();
 
+// Connect DataBase
+connectDB();
 
-app.get('/', (req, res) => {
-    res.json({"message": "Welcome to Thrive Interns"})
-})
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Thrive Interns" });
+});
 
 app.listen(3000, () => {
-    console.log('App listening on port 3000!');
+  console.log("App listening on port 3000!");
 });
